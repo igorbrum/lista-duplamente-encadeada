fa97751346d2713ac530795b150c573aaf0c78d8
Igor Brum <igorsbrum@gmail.com>
2017-07-05 14:47:15 -0300

    Adicionado metodo imprimeContrario

--- src/TestaListaEncadeada.java
+++ src/TestaListaEncadeada.java
@@ -1,21 +1,20 @@
-
-
 public class TestaListaEncadeada {
 
 	public static void main(String[] args) {
 		ListaEncadeada lista = new ListaEncadeada();
+		
+		System.out.println("LISTA");
 		lista.append("2");
 		lista.append("3");
 		lista.append("4");
 		lista.append("5");
 		lista.append("6");
 		lista.addFirst("1");
-		
 		imprime(lista);
 		
 		System.out.println("===============================");
 		System.out.println("LISTA COM INSERT ANTES E DEPOIS");
-		System.out.println("===============================");
+		
 		No iter = lista.getFirst();
 		for (int i = 0; i < 3; i++) {
 			iter = iter.next();
@@ -23,15 +22,24 @@ public class TestaListaEncadeada {
 		iter.insertBefore(new No("Antes do 4"));
 		iter.insertAfter(new No("Depois do 4"));
 		imprime(lista);
+		
+		System.out.println("===============================");
+		System.out.println("LISTA AO CONTRARIO");
+		
+		imprimeContrario(lista);	
 	}
-
 	private static void imprime(ListaEncadeada lista) {
 		No iter = lista.getFirst();
 		while (iter != null) {
 			System.out.println("Valor: "+iter.getData());
 			iter = iter.next();
-			System.out.println("----------");
 		}
 	}
-
+	private static void imprimeContrario(ListaEncadeada lista){
+		No iter = lista.getLast();
+		while (iter != null) {
+			System.out.println(iter.getData());
+			iter = iter.previous();
+		}
+	}
 }
